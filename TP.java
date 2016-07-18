fd6d6f8caaff2c3ed6ee087dde2a74ff2fc10c09
Dizga <alexandre.diz-ganito.1@etsmtl.net>
2016-07-18 19:54:57 -0400

    Update TP.java

--- TP.java
+++ TP.java
@@ -1,26 +1,7 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-import javax.swing.JFileChooser;
-import javax.swing.JPanel;
 
-/*
- * tp1
- * 
- * 
-*/
-public class TP extends JPanel{
-	static int nbVal;
-	static double mean;
-	static double variance;
-	static double deviation;
-	static double confidence;
+public class TP{
 
 	public static void main(String[] args){
-		double[] tab;
-		double t90 = 2.132;			//valeur t pour df = 4
-		double t80 = 1.533;
-		double t70 = 1.19;
 		System.out.println("Intervalle 90 : ");
 	}
 }
