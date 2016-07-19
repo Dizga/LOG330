4d95e58a80f5f4bf94824d7501dba9140f574ce1
Dizga <alexandre.diz-ganito.1@etsmtl.net>
2016-07-19 13:32:54 -0400

    Update HelloWorldTest.java

--- test/helloWorld/HelloWorldTest.java
+++ test/helloWorld/HelloWorldTest.java
@@ -19,12 +19,12 @@ public class HelloWorldTest {
 
 	@Test
 	public void testVarianceMax() {
-		double[] tab = {Math.sqrt(Double.MAX_VALUE/2),-Math.sqrt(Double.MIN_VALUE/2)};
-		assertEquals(Double.MAX_VALUE,calcul.Variance(tab),1d);
+		double[] tab = {Math.sqrt(Double.MAX_VALUE/2),-Math.sqrt(Double.MAX_VALUE/2)};
+		assertEquals(Double.MAX_VALUE/2,calcul.Variance(tab),1d);
 	}
 	@Test
 	public void testVarianceMin() {
 		double[] tab = {Math.sqrt(Double.MIN_VALUE*2),-Math.sqrt(Double.MIN_VALUE*2)};
-		assertEquals(Double.MIN_VALUE,calcul.Variance(tab),Double.MIN_VALUE);
+		assertEquals(Double.MIN_VALUE,calcul.Variance(tab),1d);
 	}
 }
