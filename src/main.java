43ded792638c8f429610bbcdd46b297286dc2575
Alexandre Ganito <alexandre.ganito@gmail.com>
2016-07-19 00:58:58 -0400

    add

--- src/main.java
+++
@@ -1,7 +0,0 @@
-package org.pbit.ci.travis.java;
-public class Main {
- 
-    public static void main(String [] args) {
-        System.out.println("hey, travis");
-    }
-}
--- test/org/pbit/ci/travis/java/TestTest.java
+++ test/org/pbit/ci/travis/java/TestTest.java
@@ -1,12 +1,13 @@
 package org.pbit.ci.travis.java;
- 
+
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
- 
+
 public class TestTest {
- 
+
     @Test
     public void testTest() {
-        assertTrue(1 == 1);
+	assertTrue(1 == 1);
     }
 }
+
